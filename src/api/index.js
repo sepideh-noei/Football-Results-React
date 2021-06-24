883f0c0f190f03b341847c5016c0c2fa9103e9a1
snoei <sepidehnoey2345@gmail.com>
2021-06-24 00:46:30 -0700

    Set the api.

---
+++ src/api/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const url = 'https://api.football-data.org/v2/competitions/2021/matches';
+
+export const fetchData = async () => {
+   try{
+        const response = await axios.get(url , {
+         headers :{ 'X-Auth-Token': 'ea7cb0230e764362aef72269a19ed9d5'}
+     }); 
+        return response;
+   }
+   catch(error){
+      console.log(error);
+   }
+}
--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+//Authorization: <type> <credentials>
+  //  Authorization: Basic YWxhZGRpbjpvcGVuc2VzYW1l
+//</credentials>
